019dc0d30ee14816cc894a25d6a246d46bfa48ce
Mukunda <mukundvishwakarma042@gmail.com>
2020-08-29 16:02:57 +0530

    FIX :: Changes in Header.php

--- header.php
+++ header.php
@@ -4,8 +4,9 @@ if(! $_SESSION['user_id']){
     header('location: login.php');
 }
 
-    if(isset($_POST['logout'])){
-        session_destroy();
+    if(isset($_POST['logout']) || isset($_POST['logout1'])){
+        $func->logoutFunction();
+        //session_destroy();
     }
 
 ?>
@@ -22,30 +23,57 @@ if(! $_SESSION['user_id']){
             <a href="index.php"><img src="" alt=""><h3>LockdownStore</h3></a>
         </div>
         <div class="humberger__menu__cart">
-        <div class="header__top__right__auth">
-                <a href="#"><i class="fa fa-user"></i> Logout</a>
-        </div>
-            
+            <ul>
+                <?php $user_id = $_SESSION['user_id'];
+                 $sql = "SELECT count(id) as total from cart where user_id=$user_id";
+                 $results = $func->query($sql);
+                 $data = $func->fetch($results);
+                 ?>
+                <li><a href="shoping-cart.php"><i class="fa fa-shopping-bag"></i> <span><?php if(empty($data['total'])){ echo '0'; }else { echo $data['total']; } ?></span></a></li>
+            </ul>
         </div>
         <div class="humberger__menu__widget">
-        <div class="header__cart__price">Cart item: <span>&#8377 150.00</span></div>
+            <div class="header__top__right__auth">
+                <?php if(isset($_SESSION['user_id']) && !empty($_SESSION['user_id'])){ ?>
+                    <li><i class="fa fa-user"></i> <?php echo $_SESSION['user_name']; ?></li>    
+                    <li><form method="post">
+                   <input type="submit" name="logout" id="logout" value="Logout" />
+                </form></li>
+                <?php }else{ ?>
+                    <li><a href="login.php"><i class="fa fa-user"></i> Login</a></li>
+                <?php } ?>
+            </div>
         </div>
         <nav class="humberger__menu__nav mobile-menu">
             <ul>
-                <li class="active"><a href="./index.html">Home</a></li>
-                <li><a href="./shop-grid.html">Products</a></li>
-                <li><a href="./shop-grid.html">About us</a></li>
-                <li><a href="./contact.html">Contact</a></li>
+                <li class="active"><a href="index.php">Home</a></li>
+                <li><a href="shoping-cart.php">Shoping Cart</a></li>
+                 <li><a href="about-us.php">About Us</a></li>
+                <!-- <li><a href="checkout.php">Check Out</a></li> -->
+                <!--<li><a href="contact.php">Contact</a></li>-->
+                <?php if(isset($_SESSION['user_id']) && !empty($_SESSION['user_id'])){ ?>
+                    <li><i class="fa fa-user"></i> <?php echo $_SESSION['user_name']; ?></li>    
+                    <li><form method="post">
+                   <input type="submit" name="logout" id="logout" value="Logout" />
+                </form></li>
+                <?php }else{ ?>
+                    <li><a href="login.php"><i class="fa fa-user"></i> Login</a></li>
+                <?php } ?>
             </ul>
         </nav>
         <div id="mobile-menu-wrap"></div>
-        
-        <div class="humberger__menu__contact">
-            <ul>
-                <li><i class="fa fa-envelope"></i> ddriversion@gmail.com</li>
-                <li>Free Shipping for all Order of &#8377 1000</li>
-            </ul>
-        </div>
+        <!--<div class="header__top__right__social">-->
+        <!--    <a href="#"><i class="fa fa-facebook"></i></a>-->
+        <!--    <a href="#"><i class="fa fa-twitter"></i></a>-->
+        <!--    <a href="#"><i class="fa fa-linkedin"></i></a>-->
+        <!--    <a href="#"><i class="fa fa-pinterest-p"></i></a>-->
+        <!--</div>-->
+        <!--<div class="humberger__menu__contact">-->
+        <!--    <ul>-->
+        <!--        <li><i class="fa fa-envelope"></i> hello@colorlib.com</li>-->
+        <!--        <li>Free Shipping for all Order of $99</li>-->
+        <!--    </ul>-->
+        <!--</div>-->
     </div>
     <!-- Humberger End -->
 
@@ -63,9 +91,10 @@ if(! $_SESSION['user_id']){
                     <nav class="header__menu">
                         <ul>
                             <li class="active"><a href="index.php">Home</a></li>
-                            <li><a href="shoping-cart.php">Shopping Cart</a></li>
-                            <li><a href="checkout.php">About Us</a></li>
-                            <li><a href="contact.php">Contact</a></li>
+                            <li><a href="shoping-cart.php">Shoping Cart</a></li>
+                            <li><a href="about-us.php">About Us</a></li>
+                            <!-- <li><a href="checkout.php">Check Out</a></li> -->
+                            <!--<li><a href="contact.php">Contact</a></li>-->
                             <?php if(isset($_SESSION['user_id']) && !empty($_SESSION['user_id'])){ ?>
                                 <li><i class="fa fa-user"></i> <?php echo $_SESSION['user_name']; ?></li>    
                                 <li><form method="post">
