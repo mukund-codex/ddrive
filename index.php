134f23e6244951bebe7f465feabea150481f3c8b
Mukunda <mukundvishwakarma042@gmail.com>
2020-08-31 20:48:54 +0530

    FIX :: Changes in Index.php

--- index.php
+++ index.php
@@ -41,7 +41,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-3">
-                    <div class="hero__categories">
+                    <div class="hero__categories" style="height: 430px;overflow-y: scroll;">
                         <div class="hero__categories__all">
                             <i class="fa fa-bars"></i>
                             <span>All Products</span>
@@ -85,7 +85,7 @@
 
                             <div class="col-lg-4 col-md-6 col-sm-6">
                             <div class="product__item">
-                                <div class="product__item__pic set-bg" data-setbg="images/product/.<?php echo $rows1['image']; ?>">
+                                <div class="product__item__pic set-bg" data-setbg="images/product/<?php echo $rows1['image']; ?>">
                                     <!-- <ul class="product__item__pic__hover">
                                         <li><a href="#"><i class="fa fa-heart"></i></a></li>
                                         <li><a href="#"><i class="fa fa-retweet"></i></a></li>
